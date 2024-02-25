d89190ed5258edf40f06ac858d9f32271cbc37f5
Ann Charity <charitykaroki1@gmail.com>
2024-02-26 01:44:58 +0300

    Add user_input.py and implement user input functionality
    This commit adds a new Python file named user_input.py. It contains code that uses the input() function to gather user information (name, age, and location) and prints a personalized message. This is part of an assignment for learning Git and Python.

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("Enter your name:")
+age = input("Enter your current age:")
+location = input("Enter your current location:")
+print(f"Hey {name},you are {age} years old and stay at {location}")
